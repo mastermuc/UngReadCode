692b122f406ffe6a8f19994352ab7b603e3461c6
muc <deawmicrop@gmail.com>
2018-03-22 12:05:49 +0700

    Post Value to Server Success

--- app/src/main/java/masterung/androidthai/in/th/ungreadcode/fragment/RegisterFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/ungreadcode/fragment/RegisterFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +14,8 @@ import android.widget.EditText;
 import masterung.androidthai.in.th.ungreadcode.MainActivity;
 import masterung.androidthai.in.th.ungreadcode.R;
 import masterung.androidthai.in.th.ungreadcode.utility.MyAlert;
+import masterung.androidthai.in.th.ungreadcode.utility.MyConstant;
+import masterung.androidthai.in.th.ungreadcode.utility.PostUserToServer;
 
 /**
  * Created by muc on 21/3/2561.
@@ -61,6 +64,34 @@ public class RegisterFragment extends Fragment{
 
                 } else {
 //                    No Space
+                    try {
+
+                        MyConstant myConstant = new MyConstant();
+                        PostUserToServer postUserToServer = new PostUserToServer(getActivity());
+                        postUserToServer.execute(nameString, userString, passwordString,
+                                myConstant.getUrlPostUserString());
+                        String result = postUserToServer.get();
+                        Log.d("22MarchV1", "Result ==>" + result);
+
+                        if (Boolean.parseBoolean(result)) {
+                            getActivity().getSupportFragmentManager().popBackStack();
+
+                        } else {
+
+                            MyAlert myAlert = new MyAlert(getActivity());
+                            myAlert.myDialog("Cannot Post User",
+                                    "Please Try Again");
+
+
+                        }
+
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+
+
 
                 }
 
---
+++ app/src/main/java/masterung/androidthai/in/th/ungreadcode/utility/MyConstant.java
@@ -0,0 +1,15 @@
+package masterung.androidthai.in.th.ungreadcode.utility;
+
+/**
+ * Created by muc on 22/3/2561.
+ */
+
+public class MyConstant {
+
+    //    About url
+    private String urlPostUserString = "http://androidthai.in.th/mar/postUser.php";
+
+    public String getUrlPostUserString() {
+        return urlPostUserString;
+    }
+}   //Main Class
